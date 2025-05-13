57d8d9683d347ccd38af301bb09bce1c18c4b205
Bryanna7 <ibarra01@colum.edu>
2025-05-13 10:11:02 -0500

    Add files via upload

---
+++ AdventureGameMapArray/AdventureGame/Location.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventureGame.Utility;
+
+namespace AdventureGame
+{
+    enum AverageTemperatureClimate: byte
+    {
+        mountain = 20,
+        forest = 50,
+        grassland = 75,
+        desert = 90,
+        volcano = 128
+
+    }
+       
+    internal class Location
+    {
+        public string LocationName;
+        public AverageTemperatureClimate TemperatureClimate = AverageTemperatureClimate.volcano;
+        //items to be found
+        //NPCs will be here too
+        List<Person> People = new List<Person>();
+        public Location()
+        {
+            People.Add(new NPC());
+            
+
+
+            List<string> prefix = new List<string>(){ "Mount","Castle", "Village of", "Lake", "Valley of", "Desert", "Realm of", "Land of" };
+            List<string> names = new List<string>() { "Awesome", "Superior", "Fabulous", "Purpleness", "Lattes", "Coffee", "Sourdough Bread", "Cupcake", "Tiramisu", "Candycane" };
+            LocationName = $"{prefix[GetRandomNumber(prefix.Count)]} {names[GetRandomNumber(names.Count)]}";
+
+        }
+
+        public void Visit()
+        {
+            //oprint out name where they are
+            Utility.Print($"You have arrived at {LocationName}.");
+
+            //NPC apper (random or dedicated?)
+
+            //NPC has a request?
+
+            //Find a randon item?
+
+            //Activity?
+
+            //Special event based on?
+
+            Pause();
+
+        
+        }
+    }
+}
---
+++ AdventureGameMapArray/AdventureGame/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    internal class Person
+    {
+        public string PersonName;
+        //list to hold items
+
+        public Person()
+        {
+                
+
+        
+        }
+            
+        List<Item> Inventory = new List<Item>();    
+          
+        public void Equip(WearableItem item)
+        { 
+            //code here tp "equip a wearable item"
+        
+        }
+
+        public void Use(Item item)
+        {
+            //code here to "equip a wearable item"
+        }
+
+
+    }
+}
